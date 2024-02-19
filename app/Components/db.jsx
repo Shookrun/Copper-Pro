d71998c5c057184a895b72d662b8fa9b6cc64a1f
Shookrun <sukranmamedov999@gmail.com>
2024-02-19 20:02:48 +0400

    Footer Up Sectiond Completed

--- app/Components/db.jsx
+++ app/Components/db.jsx
@@ -10,15 +10,19 @@ const ImageGallery = async () => {
   const {data} = await axios.get(url)
 
   return (
-      <div className='card flex flex-wrap justify-center '>
-        {data?.map(({id,title,url,content}) => (
-          <div key={id} className='card flex flex-col w-[22%] bg-blue-500'>
-           <div className='card gbox bg-red w-[20%]'>
-           <Image src={url} alt={title} width={100} height={100} />
+      <div className='my-10 flex flex-wrap gap-10 p-15 justify-center'>
+        {data?.map(({id,title,url,content,subtitle}) => (
+          <div key={id} className='flex gap-2 flex-col w-[22%]'>
+           <div className=' gbox bg-red w-[16%] p-1'>
+           <Image src={url} alt={title} width={100} height={100}  />
            </div>
-            <h3>{title}</h3>
-            <Typography>
+            <Typography variant='subtitle1' className='mont font-bold'>{title}</Typography>
+            <Typography className='mont'>
               {content}
+            </Typography>
+
+            <Typography>
+              {subtitle}
             </Typography>
           </div>
         ))}
