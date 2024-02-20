55b46a4e5b052afea699ffd1884d9bf71b4eafeb
Shookrun <sukranmamedov999@gmail.com>
2024-02-20 20:26:23 +0400

    Some Updates

--- app/Components/Footer/Firstlist.tsx
+++ app/Components/Footer/Firstlist.tsx
@@ -1,14 +1,39 @@
-import { Typography } from "@mui/material";
-
+import { Stack, Typography } from "@mui/material";
+import { FaTwitter,FaFacebookF } from "react-icons/fa";
+import { BiLogoInstagramAlt } from "react-icons/bi";
 export default function Firstlist() {
-    let arr = ["Naviqasiya", "Kataloq", "Yeniliklər", "Çatdırılma", "Bizim haqqımızda", "Əlaqə"];
+    let firstarr = ["Naviqasiya", "Kataloq", "Yeniliklər", "Çatdırılma", "Bizim haqqımızda", "Əlaqə"];
+    let secondarr = ["Kataloq","Efir yağları üçün","Hidrozollar üçün","Mis qablar","Mis aksesuarları","Fərdi sifariş","Endirimlər və təkliflər"]
+    let thirdarr = ["Əlaqə","Bazhana 8-B, Kiyev,02132 Ukrayna","+38 (096) 990 67 56","a.alambik@gmail.com"]
     return (
-        <div>
-            {arr.map((item, index) => (
-                <Typography key={index}>
-                    {item}
-                </Typography>
+        <div className="flex  justify-center gap-10 w-[100%]">
+            <Stack className="text-white mont">
+            {firstarr.map((item, index) => (
+                <ul key={index}>
+                    <li>{item}</li>
+                </ul>
             ))}
+            </Stack>
+            <Stack className="text-white mont">
+            {secondarr.map((item,index)=>(
+                <ul key={index}>
+                <li>{item}</li>
+                
+            </ul>
+            ))}
+            </Stack>
+            <Stack className="text-white mont">
+            {thirdarr.map((item,index)=>(
+                <ul key={index}>
+                <li>{item}</li>
+            </ul>
+            ))}
+            <div className="flex text-2xl ">
+            <FaTwitter className="text-white " />
+            <FaFacebookF className="text-white" />
+            <BiLogoInstagramAlt className="text-white" />
+            </div>
+            </Stack>
         </div>
     );
 }
--- app/Components/Footer/Footer.tsx
+++ app/Components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import img from "@/public/Logo.png"
-import { Stack, Typography } from "@mui/material";
+import { Container, Stack, Typography } from "@mui/material";
 import ListFirst from "./Firstlist"
 export default function Footer () {
    
 return (
     <div className="bg-[url('../public/Footer.png')]">
 
-        <Stack className="bg-red w-[17%]">
+       <Container maxWidth="lg" className=" flex justify-between">
+       <Stack className="w-[17%]">
         <Image src={img} alt="logo" width={140} height={140}/>
         <Typography >
         © 2021 “Copper Pro”
@@ -21,6 +22,7 @@ Bütün hüquqlar qorunur
 
         <ListFirst/>
         </Stack>
+       </Container>
     </div>
 )
 }
