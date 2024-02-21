0e3af7a181336065f3c406ff7ad51e00f3c8baef
Shookrun <sukranmamedov999@gmail.com>
2024-02-21 15:18:32 +0400

    Footer Updates

--- app/Components/Footer/Firstlist.tsx
+++ app/Components/Footer/Firstlist.tsx
@@ -11,24 +11,24 @@ const FirstList: FC<FirstListProps> = () => {
   let firstarr: string[] = ["Naviqasiya", "Kataloq", "Yeniliklər", "Çatdırılma", "Bizim haqqımızda", "Əlaqə"];
   let secondarr: string[] = ["Kataloq","Efir yağları üçün","Hidrozollar üçün","Mis qablar","Mis aksesuarları","Fərdi sifariş","Endirimlər və təkliflər"];
   let thirdarr: string[] = ["Əlaqə","Bazhana 8-B, Kiyev,02132 Ukrayna","+38 (096) 990 67 56","a.alambik@gmail.com"];
-
+  
   return (
     <div className="flex justify-center gap-10 w-[100%]">
-      <Stack className="text-white mont">
+      <Stack className="text-white mont gap-4">
         {firstarr.map((item, index) => (
           <ul key={index}>
             <li>{item}</li>
           </ul>
         ))}
       </Stack>
-      <Stack className="text-white mont">
+      <Stack className="text-white mont gap-4">
         {secondarr.map((item,index)=>(
           <ul key={index}>
             <li>{item}</li>
           </ul>
         ))}
       </Stack>
-      <Stack className="text-white mont">
+      <Stack className="text-white mont gap-4">
         {thirdarr.map((item,index)=>(
           <ul key={index}>
             <li>{item}</li>
--- app/Components/Footer/Footer.tsx
+++ app/Components/Footer/Footer.tsx
@@ -5,16 +5,16 @@ import ListFirst from "./Firstlist"
 export default function Footer () {
    
 return (
-    <div className="bg-[url('../public/Footer.png')]">
+    <div className="bg-[url('../public/Footer.png')] p-10">
    
        <Container  maxWidth="lg" className=" flex justify-between">
        <Stack className="w-[17%]">
         <Image src={img} alt="logo" width={140} height={140}/>
-        <Typography >
+        <Typography  className="text-white">
         © 2021 “Copper Pro”
 Bütün hüquqlar qorunur
         </Typography>
-        <Typography className="underline cursor-pointer">
+        <Typography className="text-white underline cursor-pointer">
         Gizlilik Siyasəti
         </Typography>
         </Stack>
