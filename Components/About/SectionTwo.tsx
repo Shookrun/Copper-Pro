7d2a4cc693a2f4f25055bf2375b4be8894e0e208
Shookrun <sukranmamedov999@gmail.com>
2024-03-13 20:37:17 +0400

    Add Left div

--- Components/About/SectionTwo.tsx
+++ Components/About/SectionTwo.tsx
@@ -1,9 +1,19 @@
+import { Container, Typography } from "@mui/material"
 
 const SectionTwo = () => {
   return (
-    <div>
-        SectionTwo
+    <div className="bg-white p-24">
+        <Container>
+        <div className="flex flex-col gap-4">
+            <Typography variant="h4" className="text">
+                MÜŞTƏRİLƏRİMİZ
+            </Typography>
+            <Typography className="mont w-96">
+            Biz özümüzdən tələb edirik ki, hər gün müştərilərimiz üçün daha yaxşı olaq və onların hər biri üçün ən yaxşı həllər tapaq.
+            </Typography>
         </div>
+        </Container>
+    </div>
   )
 }
 
