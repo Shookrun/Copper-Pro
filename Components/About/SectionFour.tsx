2fc8a63511ab83f055be6c3159269ec10de7a97f
Shookrun <sukranmamedov999@gmail.com>
2024-03-14 20:56:00 +0400

    SectionFour Completed

--- Components/About/SectionFour.tsx
+++ Components/About/SectionFour.tsx
@@ -1,14 +1,25 @@
 import { Typography } from "@mui/material";
-
+import Image from "next/image";
+import img from "@/public/map.svg"
 const SectionFour = () => {
   return (
-    <div className="py-32 bg-[url('../public/fourBg.png')] bg-neutral-100">
-      <div className="bg-red w-[20vw] flex flex-col justify-between h-[22vh]">
-        <Typography variant="h4" className="text">MƏKANLARIMIZ</Typography>
+    <div className=" flex items-center bg-[url('../public/fourBg.png')] bg-neutral-100">
+      <div className=" w-[20vw] ml-20 flex flex-col justify-between h-[25vh]">
+        <Typography variant="h3" className="text">MƏKANLARIMIZ</Typography>
         <Typography className="mont">
           İstehsalımız Ukraynada yerləşir, lakin 50-dən çox ölkədən olan
           müştərilər mağazamızdan alınan mallardan zövq alırlar.
         </Typography>
+      </div>
+
+      <div className=" flex justify-end pr-10 items-center h-[100vh] w-[100vw]">
+        <Image
+        src={img}
+        width={100}
+        height={100}
+        alt="map"
+        className="h-[70vh] w-[70vw] object-cover"
+        />
       </div>
     </div>
   );
