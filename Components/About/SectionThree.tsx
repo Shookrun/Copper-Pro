9d65d96cfa43f89df34f46ac534f8e87de7846bc
Shookrun <sukranmamedov999@gmail.com>
2024-03-14 19:52:31 +0400

    Some Changes

--- Components/About/SectionThree.tsx
+++ Components/About/SectionThree.tsx
@@ -3,10 +3,20 @@ import { Typography } from "@mui/material"
 
 const SectionThree = () => {
   return (
-    <div className="bg-[url('../public/SectionThree.png')] p-96 bg-cover bg-no-repeat bg-black">
-        <div className="w-[30%]">
-        <Typography></Typography>
-        </div>
+    <div className="bg-[url('../public/SectionThree.png')] flex    pt-64     bg-cover bg-no-repeat bg-black">
+        
+    
+     <div className=" flex justify-end items-end relative h-32 w-32 ">
+     <Typography variant="h1" className="w-[30%] absolute -rotate-90  top-0  text text-white">
+            COPPER PRO
+        </Typography>
+     </div>
+
+     <Typography className="text-white">
+      Salam
+     </Typography>
+      
+    
 
  </div>
   )
