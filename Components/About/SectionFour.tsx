b52293b5f0d0b5dd649307abed1e8772f47cebc4
Shookrun <sukranmamedov999@gmail.com>
2024-03-14 20:45:14 +0400

    Some updates

--- Components/About/SectionFour.tsx
+++ Components/About/SectionFour.tsx
@@ -1,9 +1,17 @@
-
+import { Typography } from "@mui/material";
 
 const SectionFour = () => {
   return (
-    <div>SectionF</div>
-  )
-}
+    <div className="py-32 bg-[url('../public/fourBg.png')] bg-neutral-100">
+      <div className="bg-red w-[20vw] flex flex-col justify-between h-[22vh]">
+        <Typography variant="h4" className="text">MƏKANLARIMIZ</Typography>
+        <Typography className="mont">
+          İstehsalımız Ukraynada yerləşir, lakin 50-dən çox ölkədən olan
+          müştərilər mağazamızdan alınan mallardan zövq alırlar.
+        </Typography>
+      </div>
+    </div>
+  );
+};
 
-export default SectionFour
+export default SectionFour;
