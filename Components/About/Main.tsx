aec18930af82781002f04e26608770269ade0145
Shookrun <sukranmamedov999@gmail.com>
2024-03-14 17:33:32 +0400

    SectionThree Created

--- Components/About/Main.tsx
+++ Components/About/Main.tsx
@@ -1,4 +1,5 @@
 import SectionOne from "./SectionOne"
+import SectionThree from "./SectionThree"
 import SectionTwo from "./SectionTwo"
 
 const Main = () => {
@@ -6,6 +7,7 @@ const Main = () => {
     <div>
         <SectionOne/>
         <SectionTwo/>
+        <SectionThree/>
     </div>
   )
 }
---
+++ Components/About/SectionThree.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material"
+
+
+const SectionThree = () => {
+  return (
+    <div className="bg-[url('../public/SectionThree.png')] p-96 bg-cover bg-no-repeat bg-black">
+        <div className="w-[30%]">
+        <Typography></Typography>
+        </div>
+
+ </div>
+  )
+}
+
+export default SectionThree
