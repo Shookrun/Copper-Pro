cace473a2aa4ab0bcb73f789b53e6df909f19c8a
Shookrun <sukranmamedov999@gmail.com>
2024-03-14 17:20:24 +0400

    SectionTwo Completed

--- Components/About/SectionTwo.tsx
+++ Components/About/SectionTwo.tsx
@@ -1,8 +1,14 @@
 "use client"
 import { Container, Typography } from "@mui/material"
-import CountUp from 'react-countup';
+import CountUp,{useCountUp} from 'react-countup';
 
 const SectionTwo = () => {
+    useCountUp({
+        ref: 'counter',
+        end: 1234567,
+        enableScrollSpy: true,
+        scrollSpyDelay: 8000,
+      });
   return (
     <div className="bg-white flex p-24">
         <Container className="flex">
@@ -15,9 +21,10 @@ const SectionTwo = () => {
             </Typography>
         </div>
 
-        <div className="w-[50%]  flex justify-center items-center ">
+        <div className="w-[50%]   flex flex-col ">
             {/* <Typography variant="h2" className="text-transparent bg-clip-text font-bold tracking-widest text-9xl bg-[url('../public/forest.webp')] bg-left ">12 346</Typography> */}
-            <CountUp duration={20} className="text-transparent bg-clip-text font-bold tracking-widest text-9xl bg-[url('../public/forest.webp')] bg-left "  end={1200} />
+            <Typography className="mont font-bold bg-clip-text text-3xl text-transparent  bg-gradient-to-r  from-gradient1 to-gradient2">Müştəri Sayğacı</Typography>
+            <CountUp  className="text-transparent bg-clip-text font-bold tracking-widest text-9xl bg-[url('../public/forest.webp')] bg-left "  end={12346} enableScrollSpy />
         </div>
         </Container>
     </div>
