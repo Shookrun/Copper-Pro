f50b05ef6bb65f25c7ec2e077dfcf2ef94bce986
Shookrun <sukranmamedov999@gmail.com>
2024-03-14 20:09:48 +0400

    SectionThree Updates

--- Components/About/SectionThree.tsx
+++ Components/About/SectionThree.tsx
@@ -1,25 +1,61 @@
-import { Typography } from "@mui/material"
-
+import { Typography } from "@mui/material";
 
 const SectionThree = () => {
   return (
-    <div className="bg-[url('../public/SectionThree.png')] flex    pt-64     bg-cover bg-no-repeat bg-black">
-        
-    
-     <div className=" flex justify-end items-end relative h-32 w-32 ">
-     <Typography variant="h1" className="w-[30%] absolute -rotate-90  top-0  text text-white">
-            COPPER PRO
+    <div className="bg-[url('../public/SectionThree.png')] flex  bg-cover bg-no-repeat bg-black">
+      <div className=" flex items-start justify-center px-24 pt-20    h-[53vh] w-36 ">
+        <Typography variant="h1" className=" -rotate-90  text text-white">
+          COPPER PRO
         </Typography>
-     </div>
+      </div>
 
-     <Typography className="text-white">
-      Salam
-     </Typography>
-      
-    
+      <div className="bg-red flex flex-col w-[60vw]">
+        <div className="bg-blue-500 flex justify-center">
+          <Typography variant="h4" className="text text-white">
+            BİZİM TARİXİMİZ
+          </Typography>
+          </div>
 
- </div>
-  )
-}
+          <div className="flex">
+            <Typography>
+              Qalet tutaşmıyın aya yağlaşoy covid mirəti mığı tutaşmıyın qağa
+              dınqı, dayna kiri səni dığlatdın qalet çırtımına yiyə dursan?
+              suruxvat qayır, sülöymən oynaxlanma səni ciji oynaxlanma
+              xışdıyaram dınqı. Dana qayır mamoğlu siyəndər pırpız baş öy a
+              bimürvət bıy əzizin ölsün sülöymən təntitmə qarantin,
+              anarıdan-bəri eşiyə çıxma sortuxla eşiy a qıvlasız tutaşmıyın
+              təntitmə göyüz nağarersan?, murtta döyğa çırtımına yiyə dursan?
+              eşiy dığlatdın dayna varıf şöytəli dayna. Göyüz qredit şöytəli
+              suruxvat çırtımına yiyə dursan? bıy əzizin ölsün bıy dərdin alem
+              anarıdan-bəri ağrın alem haay a bimürvət, a qıvlasız xışdıyaram
+              öydə otur çırtımına yiyə dursan? beyjə covid mirəti ağrın alem
+              haay ağrın alem. Sumsux şöytəli mamoğlu tova xışdıyaram bıy əzizin
+              ölsün mığı öydə otur tova, səni göyüz həncərsən ağrın alem mamoğlu
+              anarıdan-bəri qağa qalet covid mirəti, qağa sortuxla varıf
+              anarıdan-bəri həncərsən şöytəli yağlaşoy.
+            </Typography>
 
-export default SectionThree
+            <Typography>
+              Qalet tutaşmıyın aya yağlaşoy covid mirəti mığı tutaşmıyın qağa
+              dınqı, dayna kiri səni dığlatdın qalet çırtımına yiyə dursan?
+              suruxvat qayır, sülöymən oynaxlanma səni ciji oynaxlanma
+              xışdıyaram dınqı. Dana qayır mamoğlu siyəndər pırpız baş öy a
+              bimürvət bıy əzizin ölsün sülöymən təntitmə qarantin,
+              anarıdan-bəri eşiyə çıxma sortuxla eşiy a qıvlasız tutaşmıyın
+              təntitmə göyüz nağarersan?, murtta döyğa çırtımına yiyə dursan?
+              eşiy dığlatdın dayna varıf şöytəli dayna. Göyüz qredit şöytəli
+              suruxvat çırtımına yiyə dursan? bıy əzizin ölsün bıy dərdin alem
+              anarıdan-bəri ağrın alem haay a bimürvət, a qıvlasız xışdıyaram
+              öydə otur çırtımına yiyə dursan? beyjə covid mirəti ağrın alem
+              haay ağrın alem. Sumsux şöytəli mamoğlu tova xışdıyaram bıy əzizin
+              ölsün mığı öydə otur tova, səni göyüz həncərsən ağrın alem mamoğlu
+              anarıdan-bəri qağa qalet covid mirəti, qağa sortuxla varıf
+              anarıdan-bəri həncərsən şöytəli yağlaşoy.
+            </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SectionThree;
