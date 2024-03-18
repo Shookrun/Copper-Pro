1dd4347b854d6dd46dfb0321436e407e48d82c95
Shookrun <sukranmamedov999@gmail.com>
2024-03-18 15:55:13 +0400

    AboutSectionFive Completed

--- Components/About/AboutSectionFive.tsx
+++ Components/About/AboutSectionFive.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import React from 'react'
 import img from "@/public/SectionFive.svg"
 import icon1 from "@/public/icon1.svg"
+import icon2 from "@/public/icon2.svg"
+import icon3 from "@/public/icon3.svg"
 import { Container, Typography } from '@mui/material'
 const AboutSectionFive = () => {
   return (
     <div className='bg-white py-28 '>
-        <Container className='flex'>
-        <div className=' flex relative h-[72vh] w-[50%]'>
+        <Container className='flex justify-center'>
+        <div className=' flex relative w-[38%]'>
             <Image
             src={img}
             alt='Mountain'
@@ -15,7 +17,7 @@ const AboutSectionFive = () => {
             className=''
             />
         </div>
-        <div className='bg-red w-[40%] pl-4 pt-10 flex flex-col h-[72vh]'>
+        <div className=' w-[40%] pl-4 pt-10 gap-4 flex flex-col '>
             <Typography variant='h4' className='text w-[10%]'>
                 BİZİM DƏYƏRLƏR
             </Typography>
@@ -23,7 +25,7 @@ const AboutSectionFive = () => {
             İstehsalımız Ukraynada yerləşir, lakin 50-dən çox ölkədən olan müştərilər mağazamızdan alınan mallardan zövq alırlar.
             </Typography>
             <div>
-              <div>
+              <div className='flex items-center gap-4'>
                 <Image
                 src={icon1}
                 width={100}
@@ -32,6 +34,30 @@ const AboutSectionFive = () => {
                 className='w-[10%] '
                 />
                 <Typography>Səmərəlilik</Typography>
+              </div>
+            </div>
+            <div>
+              <div className='flex items-center gap-4'>
+                <Image
+                src={icon2}
+                width={100}
+                height={100}
+                alt='iconOne'
+                className='w-[10%] '
+                />
+                <Typography>Keyfiyyətli məhsul və xidmət</Typography>
+              </div>
+            </div>
+            <div>
+              <div className='flex items-center gap-4'>
+                <Image
+                src={icon3}
+                width={100}
+                height={100}
+                alt='iconOne'
+                className='w-[10%] '
+                />
+                <Typography>Çeviklik</Typography>
               </div>
             </div>
         </div>
