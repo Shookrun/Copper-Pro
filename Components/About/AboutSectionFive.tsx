14dc89b977a724d4268c88075f3b89f3e2f44e38
Shookrun <sukranmamedov999@gmail.com>
2024-03-18 15:46:05 +0400

    some changes

--- Components/About/AboutSectionFive.tsx
+++ Components/About/AboutSectionFive.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import img from "@/public/SectionFive.svg"
+import icon1 from "@/public/icon1.svg"
 import { Container, Typography } from '@mui/material'
 const AboutSectionFive = () => {
   return (
@@ -14,10 +15,25 @@ const AboutSectionFive = () => {
             className=''
             />
         </div>
-        <div className='bg-red w-[40%] flex flex-col h-[72vh]'>
-            <Typography>
-                
+        <div className='bg-red w-[40%] pl-4 pt-10 flex flex-col h-[72vh]'>
+            <Typography variant='h4' className='text w-[10%]'>
+                BİZİM DƏYƏRLƏR
             </Typography>
+            <Typography className='mont'>
+            İstehsalımız Ukraynada yerləşir, lakin 50-dən çox ölkədən olan müştərilər mağazamızdan alınan mallardan zövq alırlar.
+            </Typography>
+            <div>
+              <div>
+                <Image
+                src={icon1}
+                width={100}
+                height={100}
+                alt='iconOne'
+                className='w-[10%] '
+                />
+                <Typography>Səmərəlilik</Typography>
+              </div>
+            </div>
         </div>
         </Container>
     </div>
