89b6f84265d2dcf0378ff6aacf166a17a11ea936
Shookrun <sukranmamedov999@gmail.com>
2024-03-19 19:40:13 +0400

    Some Updates

--- Components/Catalog/SearchPage.tsx
+++ Components/Catalog/SearchPage.tsx
@@ -1,9 +1,9 @@
-"use client";
-
+"use client"
+import React, { useState } from "react";
 import { Container, Typography } from "@mui/material";
 import Image from "next/image";
-import React, { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
+import img from "@/public/not found.svg"
 interface Product {
   id: number;
   name: string;
@@ -34,28 +34,44 @@ const SearchPage: React.FC<SearchPageProps> = ({ products }) => {
             placeholder="Search for a product"
             value={searchTerm}
             onChange={handleSearch}
-            className="outline-none  transition-all duration-700 before:content-['Hello_World']"
+            className="outline-none transition-all duration-700 before:content-['Hello_World']"
           />
           <IoSearchOutline />
         </div>
         <div className="flex flex-wrap justify-between gap-14 py-20">
-          {filteredProducts.map((product) => (
-            <div key={product.id} className="flex flex-col w-[22vw] h-[50vh] ">
-              <Image
-                src={product.img}
-                width={100}
-                height={100}
-                alt="card1"
-                className="w-[100vw] h-[100%] "
-              />
-              <div className="flex  justify-between items-center">
-                <Typography className="mont w-32">{product.name}</Typography>
-                <Typography className="font-bold">55434</Typography>
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((product) => (
+              <div key={product.id} className="flex flex-col w-[22vw] h-[50vh]">
+                <Image
+                  src={product.img}
+                  width={100}
+                  height={100}
+                  alt="card1"
+                  className="w-[100vw] h-[100%]"
+                />
+                <div className="flex justify-between items-center">
+                  <Typography className="mont w-32">{product.name}</Typography>
+                  <Typography className="font-bold">55434</Typography>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+            ) 
+            : (
+                <div className=" flex flex-col  items-center  h-[70vh] w-[100vw]">
+                    <Typography variant="h4" className="mont">
+                        MƏHSUL TAPILMADI
+                    </Typography>
+             <Image
+            src={img}
+            width={100}
+            height={100}
+            alt="not found"
+            className="w-[50vw] "
+            />
+           </div>
+          )}
         </div>
-      </Container>
+          </Container>
     </div>
   );
 };
